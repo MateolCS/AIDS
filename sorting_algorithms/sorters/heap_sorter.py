8967973d50e2f5d2729853bb1c7dcd1945b244e4
MateoLCS <mateusz.namysl0318@gmail.com>
2024-03-13 12:55:08 +0100

    Fixed heap tester

--- sorting_algorithms/sorters/heap_sorter.py
+++ sorting_algorithms/sorters/heap_sorter.py
@@ -12,11 +12,11 @@ class HeapSorter(Sorter):
         right = 2 * i + 2  # right = 2*i + 2
 
         # See if left child of root exists and is greater than root
-        if left < n and array[left] > array[largest]:
+        if left < n and array[left] < array[largest]:
             largest = left
 
         # See if right child of root exists and is greater than largest so far
-        if right < n and array[right] > array[largest]:
+        if right < n and array[right] < array[largest]:
             largest = right
 
         # Change root, if needed
@@ -27,7 +27,8 @@ class HeapSorter(Sorter):
             self.heapify(array, n, largest)
 
 
-    def heap_sort(self, array):
+    def sort(self, array):
+        self.start_time = timer()
         n = len(array)
 
         # Build a maxheap.
@@ -38,6 +39,17 @@ class HeapSorter(Sorter):
         for i in range(n - 1, 0, -1):
             array[i], array[0] = array[0], array[i]  # swap
             self.heapify(array, i, 0)
+        
+        self.end_time = timer()
+
+        return array
     
     def get_info(self):
         return "Heap sorter test suite\n"
+
+
+    def get_time(self):
+        return super().get_time()
+    
+sorter = HeapSorter()
+print(sorter.sort([9, 12, 4, 5]))
