1e3b32a45506e3cb283eda6ecb25b46eaaca4241
MateoLCS <mateusz.namysl0318@gmail.com>
2024-04-22 21:12:18 +0200

    Added cycle detection

--- graphs/utils.py
+++ graphs/utils.py
@@ -18,3 +18,54 @@ def build_consequent_list(arcs):
     
     return consequent_list
 
+def find_cycle_matrix(in_matrix):
+    def dfs(vertex, parent):
+        visited.add(vertex)
+        for neighbor, connected in enumerate(in_matrix[vertex]):
+            if connected:
+                if neighbor not in visited:
+                    parent[neighbor] = vertex
+                    if dfs(neighbor, parent):
+                        return True
+                elif neighbor != parent[vertex]:
+                    cycle.append(neighbor)
+                    while vertex != neighbor:
+                        vertex = parent[vertex]
+                        cycle.append(vertex)
+                    return True
+        return False
+    
+    n = len(in_matrix)
+    visited = set()
+    parent = [-1] * n
+    cycle = []
+
+    for v in range(n):
+        if v not in visited and dfs(v, parent):
+            return cycle[::-1]
+    return None
+
+def find_cycle_dict(in_dict):
+    def dfs(vertex, parent):
+        visited.add(vertex)
+        for neighbor in in_dict.get(vertex, []):
+            if neighbor not in visited:
+                parent[neighbor] = vertex
+                if dfs(neighbor, parent):
+                    return True
+            elif neighbor != parent[vertex]:
+                cycle.append(neighbor)
+                while vertex != neighbor:
+                    vertex = parent[vertex]
+                    cycle.append(vertex)
+                return True
+        return False
+
+    visited = set()
+    parent = {}
+    cycle = []
+
+    for v in in_dict:
+        if v not in visited and dfs(v, parent):
+            return cycle[::-1]
+    return None
